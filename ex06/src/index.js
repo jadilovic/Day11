cab7eaf3dda470c88a78b53486578454a537fea3
jadilovic <adilovic79@yahoo.com>
2021-03-18 12:47:36 +0100

    adding exercise from 04 to 06

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+
+function multiplyArrayFunction(){
+    
+}
+  
+// Only change code above this line
+  
+  console.log(multiplyArrayFunction()); // Change this line
+  
+  module.exports = multiplyArrayFunction;
